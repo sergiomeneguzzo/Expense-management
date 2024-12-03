a74c8f002841f7fd3b4bc7bc09c9fd756ef202e8
sergiomeneguzzo <sergio03.mene@gmail.com>
2024-12-03 19:28:37 +0100

    ShadCn/UI Config

--- ExpenseManagmentFE/src/App.tsx
+++ ExpenseManagmentFE/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from './components/ui/button';
 
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default App;
