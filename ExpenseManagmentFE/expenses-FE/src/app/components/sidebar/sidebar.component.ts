8041ae5459bb118a55eb38bc0fb1a7f3e0b5a2e4
sergiomeneguzzo <sergio03.mene@gmail.com>
2024-12-24 18:56:26 +0100

    sidebar fix

--- ExpenseManagmentFE/expenses-FE/src/app/components/sidebar/sidebar.component.ts
+++ ExpenseManagmentFE/expenses-FE/src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from '../../entities/user';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,8 +9,11 @@ import { User } from '../../entities/user';
 })
 export class SidebarComponent {
   isClosed: boolean = true;
+
   @Output() logout = new EventEmitter<void>();
   @Input() user: User | null = null;
+
+  constructor(private authSrv: AuthService) {}
 
   toggleSidebar() {
     this.isClosed = !this.isClosed;
