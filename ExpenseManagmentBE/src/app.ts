24e91e3cd28d72b6b42d47568c7d16286d879219
sergiomeneguzzo <sergio03.mene@gmail.com>
2024-12-25 23:40:36 +0100

    .

--- ExpenseManagmentBE/src/app.ts
+++ ExpenseManagmentBE/src/app.ts
@@ -17,11 +17,21 @@ const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 200,
 };
+app.options('*', cors(corsOptions));
 
 app.use(cors(corsOptions));
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  next();
+});
 
 app.use('/api', apiRouter);
 
