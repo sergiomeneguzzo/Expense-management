3c18a9993585574a8b0ed6a808c8a7aedaa5a215
sergiomeneguzzo <sergio03.mene@gmail.com>
2024-12-25 23:50:03 +0100

    .

--- ExpenseManagmentBE/src/app.ts
+++ ExpenseManagmentBE/src/app.ts
@@ -8,10 +8,7 @@ import './utils/auth/auth-handlers';
 const app = express();
 
 const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'https://expense-management-m7iwel2ql-sergios-projects-c91d784c.vercel.app',
-  ],
+  origin: ['http://localhost:3000', 'https://saveupp.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
